ccc368ee4bad4a5fc03bde4ffdaff93f322e2587
nagesh <vayushakti.iaf@gmail.com>
2020-06-19 12:51:00 +0530

    Collections and Variables

--- practice/pycollections.py
+++ practice/pycollections.py
@@ -1,5 +1,16 @@
 # List
 fruits = ["Apple", "Banana", "Grapes", "Orange"]
+"""
 print(fruits)
 print(fruits[1])
 print(fruits[-1])
+print(len(fruits))
+
+for x in fruits:
+    print(x)
+"""
+z = 1
+while z <= len(fruits):
+    for y in fruits[len(fruits) - z]:
+        print(y)
+    z += 1
