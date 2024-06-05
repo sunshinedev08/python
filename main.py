a9b3269a4c9fb9b02c95600463f74ea12c670d05
Nagesh Purohit <vayushakti.iaf@gmail.com>
2024-06-05 14:49:41 +0530

    Changed Arguments from bool to str

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--list", help="Get the data stored in a list", type=bool, required=False)
-    parser.add_argument("--set", help="Get the data stored in a set", type=bool, required=False)
+    parser.add_argument("--list", help="Get the data stored in a list", type=str, required=False)
+    parser.add_argument("--set", help="Get the data stored in a set", type=str, required=False)
     args = parser.parse_args()
-    if args.list:
+    if str(args.list).__eq__("yes"):
         pycl = MyPyCollections().MyList()
         pycl.printData()
-    elif args.set:
+    elif str(args.set).__eq__("yes"):
         pycs = MyPyCollections.MySet()
         pycs.printData()
+    else:
+        print(parser.print_usage())
