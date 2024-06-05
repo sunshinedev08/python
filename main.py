2375b047730265b7e7c13e5b05f52f38d7612d05
sunshinedev08 <33192324+sunshinedev08@users.noreply.github.com>
2024-06-05 18:16:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--list", help="Get the data stored in a list", type=str, required=False)
     parser.add_argument("--set", help="Get the data stored in a set", type=str, required=False)
-    parser.add_argument("--dict", help="Get the data stored in a Dictionary", type=str, required=True)
+    parser.add_argument("--dict", help="Get the data stored in a Dictionary", type=str, required=False)
     args = parser.parse_args()
     if str(args.list).__eq__("yes"):
         pycl = MyPyCollections().MyList()
