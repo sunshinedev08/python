4c613103011b3bbdd1c9a0d262372464b99b4b21
Nagesh Purohit <vayushakti.iaf@gmail.com>
2024-06-05 14:40:07 +0530

    Added Arguments Support

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from pycollections import MyPyCollections
+from argparse import ArgumentParser
 
 if __name__ == "__main__":
-    pycl = MyPyCollections().MyList()
-    pycl.printData()
-    pycs = MyPyCollections.MySet()
-    pycs.printData()
+    parser = ArgumentParser()
+    parser.add_argument("--list", help="Get the data stored in a list", type=bool, required=False)
+    parser.add_argument("--set", help="Get the data stored in a set", type=bool, required=False)
+    args = parser.parse_args()
+    if args.list:
+        pycl = MyPyCollections().MyList()
+        pycl.printData()
+    elif args.set:
+        pycs = MyPyCollections.MySet()
+        pycs.printData()
