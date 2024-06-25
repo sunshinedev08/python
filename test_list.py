44b1d8d77d8dc7129c0fd536534135dcbc56798e
Nagesh Purohit <vayushakti.iaf@gmail.com>
2024-06-26 01:35:34 +0530

    Changed the Priority

--- test_list.py
+++ test_list.py
@@ -18,7 +18,7 @@ class Test_List:
     @allure.description("This Testcase will check if the given set of items in the data list are Vehicles or not")
     @allure.epic("Vehicles Management")
     @allure.parent_suite("List Management")
-    @pytest.mark.parametrize('vehicles_data', ['ByCycle', 'TriCycle', 'MotorCycle', 'Water Bottle'])
+    @pytest.mark.parametrize('vehicles_data', ['ByCycle', 'TriCycle', 'MotorCycle'])
     def test_isVehicle(vehicles_data):
         vehicles = ['Bus', 'Car', 'ByCycle', 'TriCycle', 'MotorCycle']
         assert vehicles_data in vehicles, f"{vehicles_data} is not a Vehicle"
