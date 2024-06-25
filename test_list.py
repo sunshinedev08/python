4751ec7610c5b73e8ba60ee9aad4406aa5124f21
Nagesh Purohit <vayushakti.iaf@gmail.com>
2024-06-25 23:53:10 +0530

    Pytest Support

--- test_list.py
+++ test_list.py
@@ -3,7 +3,7 @@ import pytest
 
 class Test_List:
     @staticmethod
-    @pytest.mark.parametrize('list_data', ['Apple', 'Banana', 'Parrot'])
+    @pytest.mark.parametrize('list_data', ['Apple', 'Banana', 'Orange'])
     def test_isFruit(list_data):
         fruits = ['Apple', 'Mango', 'Banana', 'Orange']
         assert list_data in fruits, f"{list_data} is not a fruit"
