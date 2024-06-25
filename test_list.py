7bde922b615520bd32e5270738ecedb420f4ea65
Nagesh Purohit <vayushakti.iaf@gmail.com>
2024-06-26 00:48:08 +0530

    New test

--- test_list.py
+++ test_list.py
@@ -13,3 +13,14 @@ class Test_List:
     def test_isFruit(list_data):
         fruits = ['Apple', 'Mango', 'Banana', 'Orange']
         assert list_data in fruits, f"{list_data} is not a fruit"
+
+    @staticmethod
+    @allure.title("Birds Check")
+    @allure.severity(allure.severity_level.BLOCKER)
+    @allure.description("This Testcase will check if the given set of items in the data list are Vehicles or not")
+    @allure.epic("Vehicles Management")
+    @allure.parent_suite("Vehicles Management")
+    @pytest.mark.parametrize('list_data', ['Bycycle', 'TriCycle', 'MotorCycle', 'Water Bottle'])
+    def test_isFruit(list_data):
+        vehicles = ['Bus', 'Car', 'ByCycle', 'TriCycle', 'MotorCycle']
+        assert list_data in vehicles, f"{list_data} is not a fruit"
