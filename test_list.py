56e7be4f8c04b7b3593215e759a3c267014fff51
Nagesh Purohit <vayushakti.iaf@gmail.com>
2024-06-26 00:58:05 +0530

    New test

--- test_list.py
+++ test_list.py
@@ -7,19 +7,19 @@ class Test_List:
     @allure.title("Fruits Check")
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.description("This Testcase will check if the given set of items in the data list are fruits or not")
-    @allure.epic("Fruits Management")
-    @allure.parent_suite("Fruits Management")
-    @pytest.mark.parametrize('list_data', ['Apple', 'Banana', 'Orange'])
-    def test_isFruit(list_data):
+    @allure.epic("Fruit Management")
+    @allure.parent_suite("List Management")
+    @pytest.mark.parametrize('fruits_data', ['Apple', 'Banana', 'Orange'])
+    def test_isFruit(fruits_data):
         fruits = ['Apple', 'Mango', 'Banana', 'Orange']
-        assert list_data in fruits, f"{list_data} is not a fruit"
+        assert fruits_data in fruits, f"{fruits_data} is not a fruit"
 
     @staticmethod
     @allure.severity(allure.severity_level.BLOCKER)
     @allure.description("This Testcase will check if the given set of items in the data list are Vehicles or not")
     @allure.epic("Vehicles Management")
-    @allure.parent_suite("Vehicles Management")
-    @pytest.mark.parametrize('list_data', ['ByCycle', 'TriCycle', 'MotorCycle', 'Water Bottle'])
-    def test_isFruit(list_data):
+    @allure.parent_suite("List Management")
+    @pytest.mark.parametrize('vehicles_data', ['ByCycle', 'TriCycle', 'MotorCycle', 'Water Bottle'])
+    def test_isFruit(vehicles_data):
         vehicles = ['Bus', 'Car', 'ByCycle', 'TriCycle', 'MotorCycle']
-        assert list_data in vehicles, f"{list_data} is not a Vehicle"
+        assert vehicles_data in vehicles, f"{vehicles_data} is not a Vehicle"
