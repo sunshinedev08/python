a41dd6886f684e11d69c5ffdacdb8ee7493e5357
Nagesh Purohit <vayushakti.iaf@gmail.com>
2024-06-26 00:40:00 +0530

    Pytest Support

--- test_list.py
+++ test_list.py
@@ -1,8 +1,14 @@
+import allure
 import pytest
 
 
 class Test_List:
     @staticmethod
+    @allure.title("Fruits Check")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.description("This Testcase will check if the given set of items in the data list are fruits or not")
+    @allure.epic("Fruits Management")
+    @allure.parent_suite("Fruits Management")
     @pytest.mark.parametrize('list_data', ['Apple', 'Banana', 'Orange'])
     def test_isFruit(list_data):
         fruits = ['Apple', 'Mango', 'Banana', 'Orange']
